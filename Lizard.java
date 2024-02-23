021c57fce354c17c22c56fd812d224b57108355a
Anir7358 <160676664+Anir7358@users.noreply.github.com>
2024-02-22 21:34:52 -0500

    Add files via upload

---
+++ Lizard.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.animal1;
+
+/**
+ *
+ * @author harry
+ */
+public class  Lizard extends Animal1 {
+    public void eat(){
+    
+}
+    public void sleep(){
+        
+    }
+    public void play(){
+        
+    }
+}
---
+++ Penguin.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.animal1;
+
+/**
+ *
+ * @author harry
+ */
+public class Penguin extends Animal1 {
+    void eat(){
+        
+    }
+    void sleep(){
+    
+    }
+    Void play(){
+        
+    }
+}
